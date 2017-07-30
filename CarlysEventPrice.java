0a5086295c1133d4283a1e230be77a51974997a4
Lisa Kilker <lisakilker@gmail.com>
2017-07-30 16:47:10 +0200

    Add files via upload

---
+++ CarlysEventPrice.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package W1D2;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author Lisa
+ */
+public class CarlysEventPrice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //integer input
+        String stringAttendees;
+        int numOfAttendees;
+        boolean isLarge = false;
+        
+        stringAttendees = JOptionPane.showInputDialog(null, "How many guests? ");
+        numOfAttendees = Integer.parseInt(stringAttendees);//takes string and turns into Int
+        
+        int result = numOfAttendees * 35;
+        
+        JOptionPane.showMessageDialog(null,"The number of guests is " + stringAttendees + ".");
+        JOptionPane.showMessageDialog(null,"The total cost per guest is $35.");
+        JOptionPane.showMessageDialog(null,"The total cost is " + "$" + result + ".");
+        
+        if (numOfAttendees>=50)
+            isLarge = true;
+        
+        if(isLarge)
+        JOptionPane.showMessageDialog(null,"Your party is large." );
+        
+        else
+        JOptionPane.showMessageDialog(null,"Your party is small.");
+        
+    }
+    
+    
+}
